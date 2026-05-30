99eaf346290b1ed2bf9ee7d9df7455f157366256
saradehi <sarahidms@gmail.com>
2026-05-30 18:14:31 -0300

    fixing weight details in card

--- client/src/components/DogDetails/Details.jsx
+++ client/src/components/DogDetails/Details.jsx
@@ -45,52 +45,80 @@ const DogDetails = ({match}) => {
 
 
     return (
-        <div className={`${style.container}`}>
-
-            {
-                isLoading === true ? <Loading></Loading> 
-                : <>
-                    <div className={`${style.button}`}>
-                        <button type="button"  style={{width: '8rem', height: '1.5rem'}} ><NavLink className={`${style.link}`} to={'/home'}>Back to home</NavLink></button>
-                    </div>
-                    <div className={`${style.containerTitle}`}>
-                        <h1>{dog.name}</h1>
-                    </div>
-                    <div className={`${style.containerSecond}`}>
-                        <img className={`${style.img}`} src={dog.image} alt={dog.name} />
-                        <div className={`${style.details}`}>
-                            <h3>TEMPERAMENTS: {dog.temperament ? dog.temperament.split(', ').map(ele => ele.trim()[0].toUpperCase()+ele.trim().slice(1)).sort(). join(', ') : dog.temperaments.map(ele => ele.name).sort().join(', ')}</h3>
-                            <h4>WEIGHT: {dog.weight_min !== '0' && dog.weight_max !== '0' ? `${dog.weight_min} - ${dog.weight_max} kg` : 'Not specified'}</h4>
-                            <h4>HEIGHT: {dog.height} cm</h4>
-                            <h4>LIFE SPAN: {!dog.life_span ? "10 - 15 years" : dog.life_span.split(' ').length > 1 ? dog.life_span : `${dog.life_span} years`}</h4>
-                        </div>
-                    </div>
-                    <br />
-                    <div>
-                        {
-                            dogId.length > 3 && 
-                            <div>
-                                <p>This dog belongs to database, it can be deleted.</p>
-                                <button type="button" value={dogId} onClick={event => handlerDelete(event)}>Delete dog</button>
-                            </div>
-                        }
-                        {
-                        warning === true && 
-                            <div className={`${style.warning}`}>
-                                <h4>You sure you want to delete this dog?</h4>
-                                <p>This action cannot be undone</p>
-                            </div>
-                        }
-                        <div className={`${style.space}`}>
-                            
-                        </div>
-                    </div>
-                </>
-            }
-
-            
-        </div>
-    )
+      <div className={`${style.container}`}>
+        {isLoading === true ? (
+          <Loading></Loading>
+        ) : (
+          <>
+            <div className={`${style.button}`}>
+              <button type="button" style={{ width: "8rem", height: "1.5rem" }}>
+                <NavLink className={`${style.link}`} to={"/home"}>
+                  Back to home
+                </NavLink>
+              </button>
+            </div>
+            <div className={`${style.containerTitle}`}>
+              <h1>{dog.name}</h1>
+            </div>
+            <div className={`${style.containerSecond}`}>
+              <img className={`${style.img}`} src={dog.image} alt={dog.name} />
+              <div className={`${style.details}`}>
+                <h3>
+                  TEMPERAMENTS:{" "}
+                  {dog.temperament
+                    ? dog.temperament
+                        .split(", ")
+                        .map(
+                          (ele) =>
+                            ele.trim()[0].toUpperCase() + ele.trim().slice(1),
+                        )
+                        .sort()
+                        .join(", ")
+                    : dog.temperaments
+                        .map((ele) => ele.name)
+                        .sort()
+                        .join(", ")}
+                </h3>
+                <h4>
+                  WEIGHT: {dog.weight ? `${dog.weight} kg` : "Not specified"}
+                </h4>
+                <h4>HEIGHT: {dog.height} cm</h4>
+                <h4>
+                  LIFE SPAN:{" "}
+                  {!dog.life_span
+                    ? "10 - 15 years"
+                    : dog.life_span.split(" ").length > 1
+                      ? dog.life_span
+                      : `${dog.life_span} years`}
+                </h4>
+              </div>
+            </div>
+            <br />
+            <div>
+              {dogId.length > 3 && (
+                <div>
+                  <p>This dog belongs to database, it can be deleted.</p>
+                  <button
+                    type="button"
+                    value={dogId}
+                    onClick={(event) => handlerDelete(event)}
+                  >
+                    Delete dog
+                  </button>
+                </div>
+              )}
+              {warning === true && (
+                <div className={`${style.warning}`}>
+                  <h4>You sure you want to delete this dog?</h4>
+                  <p>This action cannot be undone</p>
+                </div>
+              )}
+              <div className={`${style.space}`}></div>
+            </div>
+          </>
+        )}
+      </div>
+    );
 };
 
 export default DogDetails;
